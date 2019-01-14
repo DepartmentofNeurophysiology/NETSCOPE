51cfdfb3ac1489895e515abf9c61eed72e1d292e
TidoB <tido_berg@hotmail.com>
2019-01-14 12:26:01 +0100

    repo init

--- startup.m
+++ startup.m
@@ -1,5 +1,6 @@
 restoredefaultpath;
-addpath(genpath('D:\Tido\Msc-project\Functions'));
-rmpath('D:\Tido\Msc-project\Functions\old');
-addpath('D:\Tido\Msc-project\data_Tido');
 clear RESTOREDEFAULTPATH_EXECUTED;
+
+addpath(genpath('Functions'));
+rmpath('Functions\old');
+addpath('..\data_Tido');
