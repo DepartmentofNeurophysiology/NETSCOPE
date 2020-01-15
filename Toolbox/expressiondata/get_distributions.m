b648e4164b99f354057215594328aac2ef5356f7
Tido Bergmans <tido_berg@hotmail.com>
2020-01-15 14:50:12 +0100

    changed binning procedure for data distributions

--- Toolbox/expressiondata/get_distributions.m
+++ Toolbox/expressiondata/get_distributions.m
@@ -2,16 +2,14 @@ function [px,ex] = get_distributions(gem,varargin)
 %% Compute probability distributions of gene expression data
 % Compute the distributions of the transcription levels of the genes in the
 % gene expression matrix (GEM), by binning and counting the levels. Number
-% of bins is determined by Sturges' rule. Bins can be created for each gene
-% separately, or one set of bins for the entire dataset.
+% of bins is determined by Sturges' rule. The distributions data is later
+% used by the Entropy- and MI-computation functions.
 % 
 % [px,ex] = get_distributions(gem)
 % 
 % Input:
 % gem:      gene expression matrix
-% Optionally, the following Name-Value argument pairs:
-% method:   (optional) binning method - either 'uniform' or
-%           'separate' (default).
+% Optionally, the following Name-Value argument pair:
 % log:      whether to log-transform the data before binning, default is
 %           false.
 % 
@@ -23,9 +21,9 @@ options = struct();
 for i = 1:2:length(varargin)
     options.(varargin{i}) = varargin{i+1};
 end
-if ~isfield(options,'method')
-    options.method = 'separate';
-end
+%if ~isfield(options,'method')
+%    options.method = 'separate';
+%end
 if isfield(options,'log')
     if options.log
         gem = log(gem + 1);
@@ -36,22 +34,31 @@ end
 
 ngenes = size(gem,1);
 nbins = ceil(1+log2(size(gem,2)));
+%{
+Old way of choosing bins
 if strcmp(options.method,'uniform')
     [~,ex] = histcounts(gem(:),nbins);
     ex = repmat(ex,ngenes,1);
 elseif strcmp(options.method,'separate')
     ex = zeros(ngenes,nbins+1);
 end
-px = zeros(ngenes,length(ex(1,:))-1);
+%}
+px = zeros(ngenes,nbins);
+ex = zeros(ngenes,nbins+1);
 for i = 1:ngenes
     if sum(gem(i,:)) == 0
         continue;
     end
+    ex(i,:) = linspace(min(gem(i,:)),max(gem(i,:)),nbins+1);
+    px(i,:) = histcounts(gem(i,:),ex(i,:));
+    %{
+    Old way of binning
     if sum(ex(i,:)) == 0
         [px(i,:),ex(i,:)] = histcounts(gem(i,:),nbins);
     else
         px(i,:) = histcounts(gem(i,:),ex(i,:));
     end
+    %}
 end
 
 % Normalize distributions
@@ -59,5 +66,5 @@ px = px ./ repmat(sum(px,2),1,size(px,2));
 px(isnan(px)) = 0;
 
 if options.log
-    ex = exp(ex);
+    ex = exp(ex) - 1;
 end
